108c1b6ff3040b3973620b3e1babf148f96f61d3
devpedroA <pedroandradegt25@gmail.com>
2026-01-08 17:49:48 -0300

    .

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import { ThemeProvider } from "next-themes";
 import { Toaster } from "sonner";
 import "../../styles/globals.css";
+import { Analytics } from "@vercel/analytics/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -36,6 +37,7 @@ export default function RootLayout({
           {children}
           <Toaster richColors />
         </ThemeProvider>
+        <Analytics/>
       </body>
     </html>
   );
